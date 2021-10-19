545e171cda7623cad68da3e218b14a55e2ac63d6
dassshha <dzhguleva@mail.ru>
2021-10-19 19:59:20 +0300

    divide mock data from fucntions

--- js/data.js
+++ js/data.js
@@ -1,30 +1,6 @@
 import {getRandomNumber, getRandomNumberExcept} from './utils.js';
+import {COMMENTS_COUNT, PHOTO_INDEX_MAX_COUNT, NAMES, MESSAGES, DESCRIPTIONS} from './mock.js';
 
-const COMMENTS_MIN_COUNT = 1;
-const COMMENTS_MAX_COUNT = 5;
-const PHOTO_INDEX_MAX_COUNT = 25;
-const NAMES = [
-  'Иван',
-  'Мария',
-  'Ольга',
-  'Евгений',
-  'Николай',
-];
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-const DESCRIPTIONS = [
-  'Описание 1',
-  'Описание 2',
-  'Описание 3',
-  'Описание 4',
-  'Описание 5',
-];
 const createMessage = (messages) => {
   const messagesCount = getRandomNumber(1, 2);
   if (messagesCount === 1){
@@ -49,7 +25,7 @@ const createComment = (commentIndex) => {
 const createPhoto = (photoIndex) => {
   const descriptionIndex = getRandomNumber(0, DESCRIPTIONS.length - 1);
   const likesCount = getRandomNumber(15, 200);
-  const commentsCount = getRandomNumber(COMMENTS_MIN_COUNT, COMMENTS_MAX_COUNT);
+  const commentsCount = getRandomNumber(COMMENTS_COUNT.min, COMMENTS_COUNT.max);
   return {
     'id': photoIndex,
     'url': `photos/${photoIndex}.jpg`,
