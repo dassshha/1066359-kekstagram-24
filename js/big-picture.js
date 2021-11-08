7ddb3f18e5e29485a0109839b9bcb3f9b7943ec4
dassshha <dzhguleva@mail.ru>
2021-11-08 18:17:11 +0300

    add load comments feature

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,14 +1,18 @@
 import {findMiniatureData} from './utils.js';
 import {makeVisualizer} from './make-visualizer.js';
 import {photos} from './data.js';
+import {COMMENTS_AT_ONCE_LOAD_COUNT} from './constants.js';
 
 const miniatures = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureButtonClose = bigPicture.querySelector('.cancel');
-const commentCount = bigPicture.querySelector('.social__comment-count');
-const commentsLoader = bigPicture.querySelector('.comments-loader');
+const commentsCount = bigPicture.querySelector('.comments-count');
+const commentsLoaderButton = bigPicture.querySelector('.comments-loader');
 const commentTemplate = bigPicture.querySelector('.social__comment');
-const comments = bigPicture.querySelector('.social__comments');
+const commentsList = bigPicture.querySelector('.social__comments');
+const comments = commentsList.children;
+const commentsRange = bigPicture.querySelector('.social__comment-count');
+let loadedCommentsCount = COMMENTS_AT_ONCE_LOAD_COUNT;
 
 const fillCommentWithData = (data, commentsFragment) => {
   const comment = commentTemplate.cloneNode(true);
@@ -19,22 +23,35 @@ const fillCommentWithData = (data, commentsFragment) => {
   text.textContent = data.message;
   commentsFragment.append(comment);
 };
+const onLoadNewCommentsButtonClicked = () => {
+  const unloadedCommentsCount = comments.length - loadedCommentsCount;
+  const stopIndex = unloadedCommentsCount >= COMMENTS_AT_ONCE_LOAD_COUNT ? loadedCommentsCount + COMMENTS_AT_ONCE_LOAD_COUNT : loadedCommentsCount + unloadedCommentsCount;
+  for (let i = loadedCommentsCount; i < stopIndex; i++) {
+    comments[i].classList.remove('hidden');
+  }
+  loadedCommentsCount = stopIndex;
+  commentsRange.textContent = `${loadedCommentsCount} из ${comments.length} комментариев`;
+};
+const loadFirstComments = () => {
+  for (let i = COMMENTS_AT_ONCE_LOAD_COUNT; i < comments.length; i++) {
+    comments[i].classList.add('hidden');
+  }
+};
 const fillCommentsWithData = (data) => {
   const commentsFragment = document.createDocumentFragment();
   data.forEach((comment) => fillCommentWithData(comment, commentsFragment));
-  comments.appendChild(commentsFragment);
+  commentsList.appendChild(commentsFragment);
 };
 
 const clearComments = () => {
-  while (comments.firstChild) {
-    comments.removeChild(comments.firstChild);
+  while (commentsList.firstChild) {
+    commentsList.removeChild(commentsList.firstChild);
   }
 };
 
 const fillBigPictureWithData = (data) => {
   const img = bigPicture.querySelector('.big-picture__img > img');
   const likesCount = bigPicture.querySelector('.likes-count');
-  const commentsCount = bigPicture.querySelector('.comments-count');
   const description = bigPicture.querySelector('.social__caption');
   clearComments();
   img.src = data.url;
@@ -51,10 +68,10 @@ bigPictureButtonClose.addEventListener('click', bigPictureVisualizer.hide);
 miniatures.addEventListener('click', (evt) => {
   if (evt.target.matches('.picture__img')) {
     bigPictureVisualizer.show();
-    commentCount.classList.add('hidden');
-    commentsLoader.classList.add('hidden');
     const miniatureData = findMiniatureData(photos, evt.target.src);
     fillBigPictureWithData(miniatureData);
+    loadFirstComments();
   }
 });
 
+commentsLoaderButton.addEventListener('click', onLoadNewCommentsButtonClicked);
--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,6 @@
 const MAX_COMMENT_LENGTH = 140;
 const MAX_HASHTAGS_COUNT = 5;
+const COMMENTS_AT_ONCE_LOAD_COUNT = 5;
 const ERRORS = {
   UNIQUE_ERROR: 'Один и тот же хэш-тег не может быть использован дважды!',
   MAX_COUNT_ERROR: 'Нельзя указать больше пяти хэш-тегов!',
@@ -7,4 +8,4 @@ const ERRORS = {
   MAX_LENGTH_ERROR: 'Длина комментария не может составлять больше 140 символов!',
 };
 
-export {MAX_COMMENT_LENGTH, MAX_HASHTAGS_COUNT, ERRORS};
+export {MAX_COMMENT_LENGTH, MAX_HASHTAGS_COUNT, ERRORS, COMMENTS_AT_ONCE_LOAD_COUNT};
