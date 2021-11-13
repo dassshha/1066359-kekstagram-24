df83a0476cd266a57a262ed5908a39bec5003acd
dassshha <dzhguleva@mail.ru>
2021-11-13 20:54:58 +0300

    change importing img and scaleField to declaring

--- js/form.js
+++ js/form.js
@@ -1,10 +1,11 @@
 import {makeVisualizer} from './make-visualizer.js';
-import {scaleField, img} from './scale.js';
 import {slider} from './slider.js';
 
 const fileLoadButton = document.querySelector('#upload-file');
 const photoEditForm = document.querySelector('.img-upload__overlay');
 const closeFormButton = photoEditForm.querySelector('#upload-cancel');
+const scaleField = document.querySelector('.scale__control--value');
+const img = document.querySelector('.img-upload__preview');
 
 const formVisualizer = makeVisualizer(photoEditForm);
 
